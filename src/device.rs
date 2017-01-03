6b0ba1e60501a8cf409bce3bb9369c3b92c5ac2e
Valentin Lorentz <progval+git@progval.net>
2017-01-03 18:27:31 +0100

    Add Device::ipv4_link_del

--- src/device.rs
+++ src/device.rs
@@ -4,6 +4,7 @@ use std::ffi::CString;
 use picotcp_sys::pico_device;
 use picotcp_sys::pico_device_init;
 use picotcp_sys::pico_ipv4_link_add;
+use picotcp_sys::pico_ipv4_link_del;
 use picotcp_sys::PICO_SIZE_ETH;
 
 use error::{PicoError, get_res};
@@ -42,14 +43,42 @@ impl Device {
     /// use rustotcp::ipv4::Ipv4;
     /// rustotcp::init();
     /// let mut dev = Device::new("eth0", None);
-    /// dev.ipv4_link_add(Ipv4::from_string("192.168.1.1").unwrap(), Ipv4::from_string("255.255.255.0").unwrap());
+    /// assert_eq!(dev.ipv4_link_add(Ipv4::from_string("192.168.1.1").unwrap(), Ipv4::from_string("255.255.255.0").unwrap()), Ok(()));
     /// ```
     pub fn ipv4_link_add(&mut self, ip4: Ipv4, netmask: Ipv4) -> Result<(), PicoError> {
         match get_res(unsafe { pico_ipv4_link_add(&mut self.0 as *mut _, ip4.into(), netmask.into()) }) {
             Ok(_) => Ok(()),
             Err(e @ PicoError::NotEnoughMemory) |
             Err(e @ PicoError::NetworkUnreachable) |
-            Err(e @PicoError::HostIsUnreachable) => Err(e),
+            Err(e @ PicoError::HostIsUnreachable) => Err(e),
+            Err(res) => panic!(format!("Unexpected error from pico_ipv4_link_add: {:?}", res)),
+        }
+    }
+
+    /// `pico_ipv4_link_del`
+    ///
+    /// ```
+    /// # use rustotcp::device::Device;
+    /// use rustotcp::ipv4::Ipv4;
+    /// rustotcp::init();
+    /// let mut dev = Device::new("eth0", None);
+    /// dev.ipv4_link_add(Ipv4::from_string("192.168.1.1").unwrap(), Ipv4::from_string("255.255.255.0").unwrap()).unwrap();
+    /// assert_eq!(dev.ipv4_link_del(Ipv4::from_string("192.168.1.1").unwrap()), Ok(()));
+    /// ```
+    ///
+    /// ```
+    /// # use rustotcp::device::Device;
+    /// use rustotcp::error::PicoError;
+    /// use rustotcp::ipv4::Ipv4;
+    /// rustotcp::init();
+    /// let mut dev = Device::new("eth0", None);
+    /// dev.ipv4_link_add(Ipv4::from_string("192.168.1.1").unwrap(), Ipv4::from_string("255.255.255.0").unwrap()).unwrap();
+    /// assert_eq!(dev.ipv4_link_del(Ipv4::from_string("192.168.2.1").unwrap()), Err(PicoError::NoSuchDeviceOrAddress));
+    /// ```
+    pub fn ipv4_link_del(&mut self, ip4: Ipv4) -> Result<(), PicoError> {
+        match get_res(unsafe { pico_ipv4_link_del(&mut self.0 as *mut _, ip4.into()) }) {
+            Ok(_) => Ok(()),
+            Err(PicoError::NoSuchDeviceOrAddress) => Err(PicoError::NoSuchDeviceOrAddress),
             Err(res) => panic!(format!("Unexpected error from pico_ipv4_link_add: {:?}", res)),
         }
     }
